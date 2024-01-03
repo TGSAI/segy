3a44f8edc64ce9dc47bc929742376c1362883b5f
Altay Sansal <altay.sansal@tgs.com>
2024-01-03 00:34:37 -0600

    Enhance type annotations and docstrings in SEG-Y registry
    Updated function signatures in the SEG-Y specifications registry module, adding type hints for increased clarity. Additionally, docstrings are included for each function explaining their purpose and usage, improving in-code documentation. These changes ensure greater transparency and readability in the code.

--- src/segy/standards/registry.py
+++ src/segy/standards/registry.py
@@ -1,17 +1,22 @@
+"""Implements a registry for various SEG-Y standards."""
+
+
 from segy.schema.segy import SegyDescriptor
 from segy.schema.segy import SegyStandard
 
 registry = {}
 
 
-def register_spec(spec_type: SegyStandard, spec_cls: type[SegyDescriptor]):
+def register_spec(spec_type: SegyStandard, spec_cls: type[SegyDescriptor]) -> None:
+    """Register a SEG-Y standard with its descriptor."""
     if not issubclass(spec_cls, SegyDescriptor):
         msg = "spec_cls must be a subclass of SegyDescriptor."
         raise ValueError(msg)
     registry[spec_type] = spec_cls
 
 
-def get_spec(spec_type: SegyStandard):
+def get_spec(spec_type: SegyStandard) -> SegyDescriptor:
+    """Get a registered SEG-Y standard's descriptor."""
     spec = registry.get(spec_type)
 
     if not spec:
