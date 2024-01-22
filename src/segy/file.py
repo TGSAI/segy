6c0f8d5df087c056962e0ae7f7ecd8564ad08a8d
Altay Sansal <altay.sansal@tgs.com>
2024-01-22 15:11:06 -0600

    Refactor segy file handler for clarity and settings management
    Updated the segy file handler to make code more clear and improve settings management. The change includes removing the "postprocess_kwargs" and replacing it with a "settings" parameter. This new setting provides a much clearer structure for data processing configuration. There's now an implemented conditional byte swapping based on endianness provided by the settings.

--- src/segy/file.py
+++ src/segy/file.py
@@ -1,23 +1,29 @@
 """SEG-Y file in-memory constructors."""
 from __future__ import annotations
 
-
+from functools import cached_property
+from typing import TYPE_CHECKING
 from typing import Any
 
 import numpy as np
-from fsspec import AbstractFileSystem
 from fsspec.core import url_to_fs
-from numpy.typing import NDArray
 from pandas import DataFrame
 
+from segy.config import SegyFileSettings
 from segy.indexing import AbstractIndexer
 from segy.indexing import DataIndexer
 from segy.indexing import HeaderIndexer
 from segy.indexing import TraceIndexer
+from segy.schema import Endianness
 from segy.schema import SegyDescriptor
 from segy.schema import SegyStandard
 from segy.standards.registry import get_spec
 from segy.standards.registry import register_spec
+from segy.standards.rev1 import BinaryHeaderDescriptorRev1
+
+if TYPE_CHECKING:
+    from fsspec import AbstractFileSystem
+    from numpy.typing import NDArray
 
 
 class SegyFile:
@@ -29,33 +35,30 @@ class SegyFile:
     def __init__(
         self,
         url: str,
-        segy_standard: SegyStandard = SegyStandard.REV1,
-        pandas_headers: bool = True,
+        spec: type[SegyDescriptor] | None = None,
+        settings: SegyFileSettings | None = None,
     ):
         """Parse some metadata and get file ready for manipulating."""
+        if settings is None:
+            self.settings = SegyFileSettings()
+
         self.fs, self.url = url_to_fs(url)
 
-        # Validate standard
-        standard = SegyStandard(segy_standard)
-
-        self.spec = get_spec(standard)
-        self._postprocess_kwargs = {"pandas_headers": pandas_headers}
+        self.spec = self._infer_standard() if spec is None else spec
 
         self._info = self.fs.info(self.url)
-        self._binary_header = None
-        self._read_binary_header()
         self._parse_binary_header()
 
     @classmethod
     def from_spec(
-        cls: type["SegyFile"],
+        cls: type[SegyFile],
         url: str,
         spec: type[SegyDescriptor],
         **kwargs: dict[str, Any],
-    ) -> "SegyFile":
+    ) -> SegyFile:
         """Open a SEG-Y file based on custom spec."""
         register_spec(SegyStandard.CUSTOM, spec)
-        return cls(url=url, segy_standard=SegyStandard.CUSTOM, **kwargs)
+        return cls(url=url, spec=spec, **kwargs)
 
     @property
     def file_size(self) -> int:
@@ -65,15 +68,25 @@ class SegyFile:
     @property
     def num_traces(self) -> int:
         """Return number of traces in file based on size and spec."""
-        # TODO(Altay): Not counting extended headers for now
+        rev = self.spec.segy_standard
+        file_textual_hdr_size = self.spec.text_file_header.itemsize
+        file_bin_hdr_size = self.spec.text_file_header.itemsize
         trace_size = self.spec.trace.itemsize
-        file_metadata_size = (
-            self.spec.text_file_header.itemsize + self.spec.binary_file_header.item_size
-        )
+
+        if self.settings.BINARY.EXTENDED_TEXT_HEADER.VALUE is None and rev.value > 0:
+            header_key = self.settings.BINARY.EXTENDED_TEXT_HEADER.KEY
+            num_ext_text = self.binary_header[header_key]
+        else:
+            num_ext_text = self.settings.BINARY.EXTENDED_TEXT_HEADER.VALUE
+
+        file_metadata_size = file_textual_hdr_size + file_bin_hdr_size
+
+        if num_ext_text is not None:
+            file_metadata_size += num_ext_text * self.spec.extended_text_header.itemsize
 
         return (self.file_size - file_metadata_size) // trace_size
 
-    @property
+    @cached_property
     def text_header(self) -> str:
         """Return textual file header."""
         text_hdr_desc = self.spec.text_file_header
@@ -87,16 +100,24 @@ class SegyFile:
         text_header = text_hdr_desc._decode(buffer)
         return text_hdr_desc._wrap(text_header)
 
-    @property
+    def _infer_standard(self) -> SegyDescriptor:
+        if self.settings.REVISION is None:
+            rev1_bin_spec = BinaryHeaderDescriptorRev1()
+            buffer = self.fs.read_block(
+                fn=self.url,
+                offset=rev1_bin_spec.offset,
+                length=rev1_bin_spec.item_size,
+            )
+            revision = np.frombuffer(buffer, offset=300, dtype=">i2", count=1).item()
+
+        else:
+            revision = self.settings.REVISION
+
+        standard = SegyStandard(revision)
+        return get_spec(standard)
+
+    @cached_property
     def binary_header(self) -> NDArray[Any] | DataFrame:
-        """Return binary header as struct or pandas DataFrame."""
-        using_pandas = self._postprocess_kwargs.get("pandas_headers", False)
-        if using_pandas:
-            return DataFrame(self._binary_header.reshape(1))
-
-        return self._binary_header
-
-    def _read_binary_header(self) -> None:
         """Read binary header from store, based on spec."""
         buffer = bytearray(
             self.fs.read_block(
@@ -106,42 +127,44 @@ class SegyFile:
             )
         )
 
-        self._binary_header = (
-            np.frombuffer(buffer, dtype=self.spec.binary_file_header.dtype)
-            .squeeze()
-            .byteswap(inplace=True)
-            .newbyteorder()
-        )
+        bin_hdr = np.frombuffer(buffer, dtype=self.spec.binary_file_header.dtype)
 
-    def _parse_binary_header(
-        self,
-        samples_per_trace_key: str = "samples_per_trace",
-        extended_textual_headers_key: str = "extended_textual_headers",
-    ) -> None:
-        """Parse the binary header and apply some rules.
+        if self.settings.ENDIAN == Endianness.BIG:
+            bin_hdr = bin_hdr.byteswap(inplace=True).newbyteorder()
 
-        It currently supports:
-        - Get number of samples and update spec.
+        if self.settings.USE_PANDAS:
+            return DataFrame(bin_hdr.reshape(1))
 
-        Args:
-            samples_per_trace_key: The key to access the number of samples per trace
-                in the binary header. Default is "samples_per_trace".
-            extended_textual_headers_key: The key to get the number of extended
-                textual headers in the binary header. Default is
-                "extended_textual_headers".
-        """
+        return bin_hdr.squeeze()
+
+    def _parse_binary_header(self) -> None:
+        """Parse the binary header and apply some rules."""
         # Extract number of samples and extended text headers.
-        samples_per_trace = self.binary_header[samples_per_trace_key]
-        num_ext_text = self.binary_header[extended_textual_headers_key]
+        rev = self.spec.segy_standard
+
+        if self.settings.BINARY.SAMPLES_PER_TRACE.VALUE is None:
+            header_key = self.settings.BINARY.SAMPLES_PER_TRACE.KEY
+            samples_per_trace = self.binary_header[header_key]
+        else:
+            samples_per_trace = self.settings.BINARY.SAMPLES_PER_TRACE.VALUE
 
         # Calculate sizes for dynamic file metadata
         text_hdr_size = self.spec.text_file_header.itemsize
         bin_hdr_size = self.spec.binary_file_header.itemsize
-        ext_text_size = self.spec.extended_text_header.itemsize * int(num_ext_text)
 
         # Update trace start offset and sample length
         self.spec.trace.data_descriptor.samples = int(samples_per_trace)
-        self.spec.trace.offset = text_hdr_size + bin_hdr_size + ext_text_size
+        self.spec.trace.offset = text_hdr_size + bin_hdr_size
+
+        if self.settings.BINARY.EXTENDED_TEXT_HEADER.VALUE is None and rev.value > 0:
+            header_key = self.settings.BINARY.EXTENDED_TEXT_HEADER.KEY
+            num_ext_text = self.binary_header[header_key]
+        else:
+            num_ext_text = self.settings.BINARY.EXTENDED_TEXT_HEADER.VALUE
+
+        if num_ext_text is not None:
+            ext_text_size = self.spec.extended_text_header.itemsize * int(num_ext_text)
+            self.spec.trace.offset = self.spec.trace.offset + ext_text_size
 
     @property
     def data(self) -> AbstractIndexer:
@@ -163,7 +186,7 @@ class SegyFile:
             self.spec.trace,
             self.num_traces,
             kind="header",
-            postprocess_kwargs=self._postprocess_kwargs,
+            settings=self.settings,
         )
 
     @property
@@ -175,5 +198,5 @@ class SegyFile:
             self.spec.trace,
             self.num_traces,
             kind="trace",
-            postprocess_kwargs=self._postprocess_kwargs,
+            settings=self.settings,
         )
