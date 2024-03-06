9eaa4edf6c78a274c4cf5bd4c60b75d71a8de945
Brian Kohan <bckohan@gmail.com>
2024-03-05 16:01:07 -0800

    Fix sphinx-autobuild infinite loop bug (#31)
    * Fix sphinx-autobuild infinite loop bug
    
    * satisfy linter
    
    * satisfy linter - try2
    
    * satisfy linter - try 3
    
    ---------
    
    Co-authored-by: Altay Sansal <tasansal@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -216,7 +216,11 @@ def docs_build(session: Session) -> None:
 @session(python=python_versions[0])
 def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
-    args = session.posargs or ["--open-browser", "docs", "docs/_build"]
+    ignore = [
+        "docs/jupyter_execute/tutorials/quickstart.ipynb",
+    ]
+    args = ["--open-browser", "docs", "docs/_build", "--ignore", *ignore]
+    args = session.posargs or args
     session.install(".")
     session.install(
         "sphinx",
