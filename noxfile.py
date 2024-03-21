d869f5403a84848e0390cff6eabb39575ad8ccf9
Altay Sansal <altay.sansal@tgs.com>
2024-03-20 19:17:15 -0500

    Add s3fs dependency to typeguard because it runs tests

--- noxfile.py
+++ noxfile.py
@@ -169,7 +169,7 @@ def coverage(session: Session) -> None:
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard", "pygments")
+    session.install("pytest", "typeguard", "pygments", "s3fs")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
