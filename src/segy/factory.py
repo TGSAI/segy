26bd0841d299e18dc877057751855ca21d7a4123
Altay Sansal <altay.sansal@tgs.com>
2024-03-30 18:08:45 -0500

    Adjust SEGY revision setting in factory
    The code has been modified to only set the SEGY revision value if it is not equal to REV0. This avoids unnecessary operations and grants more efficiency when handling SEGY standards, primarily when the REV0 standard is in use. This change is applied within the binary header configuration of the factory module in SEGY.

--- src/segy/factory.py
+++ src/segy/factory.py
@@ -110,7 +110,9 @@ class SegyFactory:
         binary_descriptor = self.spec.binary_file_header
         bin_header = np.zeros(shape=1, dtype=binary_descriptor.dtype)
 
-        bin_header["seg_y_revision"] = self.segy_revision.value * 256
+        if self.segy_revision != SegyStandard.REV0:
+            bin_header["seg_y_revision"] = self.segy_revision.value * 256
+
         bin_header["sample_interval"] = self.sample_interval
         bin_header["sample_interval_orig"] = self.sample_interval
         bin_header["samples_per_trace"] = self.samples_per_trace
