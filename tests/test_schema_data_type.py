44fe6a6c1b8f2cbedc8b8d3ec56a119a57985625
Altay Sansal <altay.sansal@tgs.com>
2024-04-01 23:24:27 -0500

    Add tests for SEGY schema data types
    New test file `test_schema_data_type.py` has been created to validate components that define fields and data types of a SEGY Schema. This includes tests for data type descriptor creation, structured data type descriptor functionality, and validation of StructuredDataTypeDescriptor's recreation from JSON.

---
+++ tests/test_schema_data_type.py
@@ -0,0 +1,88 @@
+"""Tests for components that define fields and data types of a SEGY Schema."""
+
+from __future__ import annotations
+
+import numpy as np
+import pytest
+
+from segy.schema.data_type import DataTypeDescriptor
+from segy.schema.data_type import Endianness
+from segy.schema.data_type import ScalarType
+from segy.schema.data_type import StructuredDataTypeDescriptor
+from segy.schema.data_type import StructuredFieldDescriptor
+
+
+class TestBaseDataTypes:
+    """Tests for basic and structured data types and their dtype method."""
+
+    @pytest.mark.parametrize(
+        ("format_", "expected"),
+        [
+            (ScalarType.IBM32, "uint32"),
+            (ScalarType.INT64, "int64"),
+            (ScalarType.UINT16, "uint16"),
+            (ScalarType.UINT8, "uint8"),
+            (ScalarType.FLOAT32, "float32"),
+        ],
+    )
+    def test_data_type_descriptor(self, format_: ScalarType, expected: str) -> None:
+        """Test creating data type descriptors for `ScalarType`s."""
+        dtype_descr = DataTypeDescriptor(format=format_)
+        assert dtype_descr.dtype == np.dtype(expected)
+
+    @pytest.mark.parametrize("endianness", [Endianness.BIG, Endianness.LITTLE])
+    @pytest.mark.parametrize(
+        ("names", "offsets", "formats", "itemsize"),
+        [
+            (["f1"], [0], ["int32"], None),
+            (["f1", "f2"], [0, 8], ["uint8", "int32"], None),
+            (["f1", "f2"], [0, 12], ["uint8", "int16"], 20),
+        ],
+    )
+    def test_structured_data_type_descriptor(  # noqa: PLR0913
+        self,
+        endianness: Endianness,
+        names: list[str],
+        offsets: list[int],
+        formats: list[int],
+        itemsize: int,
+    ) -> None:
+        """Test StructuredDataTypeDescriptor and its dtype attribute."""
+        fields = [
+            StructuredFieldDescriptor(name=name, offset=offset, format=format_)
+            for name, offset, format_ in zip(names, offsets, formats)
+        ]
+
+        struct_descriptor = StructuredDataTypeDescriptor(
+            fields=fields, item_size=itemsize, endianness=endianness
+        )
+
+        assert struct_descriptor.dtype.names == tuple(names)
+        if endianness == Endianness.LITTLE:
+            assert struct_descriptor.dtype.isnative
+        if endianness == Endianness.BIG:
+            assert not struct_descriptor.dtype.isnative
+        if itemsize is not None:
+            assert struct_descriptor.dtype.itemsize == itemsize
+
+
+def test_structured_data_type_descriptor_json_validate() -> None:
+    """Test for validating recreating a StrucrutedDataTypeDescriptor from a JSON string."""
+    struct_json = """
+    {
+      "description": "dummy description",
+      "fields": [
+        {"description": "field1", "format": "int32", "name": "f1", "offset": 0},
+        {"description": "field2", "format": "ibm32", "name": "f2", "offset": 4}
+      ],
+      "itemSize": 12,
+      "offset": 200,
+      "endianness": "big"
+    }
+    """
+    actual_model = StructuredDataTypeDescriptor.model_validate_json(struct_json)
+    assert actual_model.description == "dummy description"
+    assert len(actual_model.fields) == 2  # noqa: PLR2004
+    assert actual_model.dtype.names == ("f1", "f2")
+    assert actual_model.dtype.itemsize == 12  # noqa: PLR2004
+    assert not actual_model.dtype.isnative
