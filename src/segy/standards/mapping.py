a1adc5bece75c631c44ee91dd2bb7967e1cdf30e
Altay Sansal <altay.sansal@tgs.com>
2024-04-02 10:59:04 -0500

    Replace custom BiDict with bidict library
    The custom BiDict class in `src/segy/standards/mapping.py` has been replaced with the external bidict library for cleaner code and improved efficiency. The bidict library was also added to the dependencies in `pyproject.toml`. This move simplifies the codebase by utilising a well-tested external library, thus removing the need for maintaining the custom BiDict implementation.

--- src/segy/standards/mapping.py
+++ src/segy/standards/mapping.py
@@ -2,54 +2,11 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-from typing import TypeVar
+from bidict import bidict
 
 from segy.schema import ScalarType
 
-if TYPE_CHECKING:
-    from collections.abc import Iterable
-
-K = TypeVar("K")
-V = TypeVar("V")
-
-
-class BiDict(dict):
-    """Bidirectional dictionary."""
-
-    def __init__(self, *args: Iterable[tuple[K, V]], **kwargs: V) -> None:
-        super().__init__(*args, **kwargs)
-        self.reverse = {v: k for k, v in self.items()}
-
-    def __setitem__(self, key: K, value: V) -> None:
-        """Set a new key in bidirectional dictionary."""
-        if value in self.reverse:
-            msg = "Value already exists with a different key"
-            raise ValueError(msg)
-        super().__setitem__(key, value)
-        self.reverse[value] = key
-
-    def __delitem__(self, key: K) -> None:
-        """Delete key from dictionary."""
-        value = self[key]
-        super().__delitem__(key)
-        del self.reverse[value]
-
-    def __contains__(self, item: K | V) -> bool:
-        """Return True if item is in bidirectional dictionary."""
-        return super().__contains__(item) or item in self.reverse
-
-    def update(self, *args: Iterable[tuple[K, V]], **kwargs: V) -> None:
-        """Update the bidirectional dictionary."""
-        for k, v in dict(*args, **kwargs).items():
-            self.__setitem__(k, v)
-
-    def inverse(self, value: V) -> K:
-        """Get key by value."""
-        return self.reverse[value]
-
-
-SEGY_FORMAT_MAP = BiDict(
+SEGY_FORMAT_MAP = bidict(
     {
         ScalarType.IBM32: 1,
         ScalarType.INT32: 2,
