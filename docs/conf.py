11f01e8898d5623db7587b5f97dbf7f5436a1940
Altay Sansal <tasansal@users.noreply.github.com>
2024-06-05 15:32:44 -0500

    Simplify JSON display in docs: quickstart (#122)
    * simplify json dump
    
    * auto execute notebooks to ensure they still work during docs build.
    
    * Remove size limit argument for large files since we emptied out notebooks.
    
    * added missing deps for RtD
    
    ---------
    
    Co-authored-by: Altay Sansal <altay.sansal@tgs.com>

--- docs/conf.py
+++ docs/conf.py
@@ -83,4 +83,6 @@ copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: 
 copybutton_line_continuation_character = "\\"
 copybutton_prompt_is_regexp = True
 
-nb_execution_mode = "off"
+nb_execution_mode = "auto"
+nb_execution_in_temp = True
+nb_execution_timeout = 60
