d08dd3691b5146d043bd4f8dc8af3561964eef48
Altay Sansal <altay.sansal@tgs.com>
2024-06-13 16:30:15 -0500

    Disable showing locals in pretty exceptions

--- src/segy/cli/segy.py
+++ src/segy/cli/segy.py
@@ -4,6 +4,8 @@ import typer
 
 from segy.cli import dump as dump_command
 
-app = typer.Typer(name="segy", no_args_is_help=True)
+app = typer.Typer(
+    name="segy", no_args_is_help=True, pretty_exceptions_show_locals=False
+)
 
 app.add_typer(dump_command.app)
