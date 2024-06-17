5f6ada4153a3fcaddcc51d8ceaef38bc53437fdb
Altay Sansal <altay.sansal@tgs.com>
2024-06-17 15:12:43 -0500

    Add tests for HeaderArray get/set items with alias

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -1,8 +1,12 @@
 """Test Numpy array subclasses."""
 
 import numpy as np
+import pytest
 
+from segy.arrays import HeaderArray
 from segy.arrays import SegyArray
+from segy.exceptions import InvalidFieldError
+from segy.exceptions import NonSpecFieldError
 
 
 def test_segy_array_copy() -> None:
@@ -22,3 +26,57 @@ def test_segy_array_copy() -> None:
 
     assert segy_array_copy.tobytes() == buffer_expected
     assert np.may_share_memory(segy_array_copy, segy_array) is False
+
+
+@pytest.mark.parametrize(
+    "keys,aliases,values",
+    [
+        (["samples_per_trace", "sample_interval"], ["ns", "interval"], (5, 2)),
+        (["samples_per_trace", "sample_interval"], ["Samples", "dt"], (1, 6)),
+    ],
+)
+class TestHeaderAlias:
+    """Test header alias get/set item methods and exceptions."""
+
+    def test_getitem_alias(
+        self, keys: list[str], aliases: list[str], values: list[int]
+    ) -> None:
+        """Test getting values by SU and segyio aliases."""
+        array = np.asarray(values, dtype="int16")
+        buffer = bytearray(array.tobytes())
+        dtype = np.dtype({"names": keys, "formats": ["int16", "int16"]})
+        struct = np.frombuffer(buffer, dtype=dtype)
+        header_array = HeaderArray(struct)
+
+        # str branch tests
+        assert header_array[aliases[0]].item() == values[0]
+        assert header_array[aliases[1]].item() == values[1]
+        with pytest.raises(InvalidFieldError, match="Invalid key"):
+            _ = header_array["foo"]
+        with pytest.raises(NonSpecFieldError, match="spec does not define this field"):
+            _ = header_array["SEGYRevision"]
+
+        # list[str branch] tests
+        assert header_array[aliases].item() == values
+        with pytest.raises(NonSpecFieldError, match="spec does not define this field"):
+            _ = header_array[["SEGYRevision", "ExtSamples"]]
+
+    def test_setitem_alias(
+        self, keys: list[str], aliases: list[str], values: list[int]
+    ) -> None:
+        """Test setting values by SU and segyio aliases."""
+        array = np.zeros(len(values), dtype="int16")
+        buffer = bytearray(array.tobytes())
+        dtype = np.dtype({"names": keys, "formats": ["int16", "int16"]})
+        struct = np.frombuffer(buffer, dtype=dtype)
+        header_array = HeaderArray(struct)
+
+        # str branch tests
+        header_array[aliases[0]] = values[0]
+        header_array[aliases[1]] = values[1]
+        assert header_array[aliases].item() == values
+
+        # list[str branch] tests
+        values_x2 = tuple(val * 2 for val in values)
+        header_array[aliases] = values_x2
+        assert header_array[aliases].item() == values_x2
