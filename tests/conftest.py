91841f2d342dc0573c7f842dfbab3e9f22cc6d8b
Altay Sansal <altay.sansal@tgs.com>
2024-06-18 16:13:33 -0500

    Add new pytest fixture for default text header

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,21 @@ from __future__ import annotations
 import pytest
 from fsspec.implementations.memory import MemoryFileSystem
 
+from segy.factory import get_default_text
+from segy.standards import get_segy_standard
+
 
 @pytest.fixture(scope="session")
 def mock_filesystem() -> MemoryFileSystem:
     """Return a mocked filesystem implementation in memory."""
     return MemoryFileSystem()
+
+
+@pytest.fixture(scope="session")
+def default_text() -> str:
+    """Get the default text header to be used in comparisons downstream."""
+    spec = get_segy_standard(1.0)
+    spec.trace.data.samples = 101
+    spec.trace.data.interval = 4000
+
+    return get_default_text(spec)
