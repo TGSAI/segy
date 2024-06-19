b362ac0ef07cd1fa48abda737a78af5d576adc56
Altay Sansal <altay.sansal@tgs.com>
2024-06-19 11:26:32 -0500

    timezone python <3.11 compatibility

--- src/segy/factory.py
+++ src/segy/factory.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
-from datetime import UTC
 from datetime import datetime
+from datetime import timezone
 from typing import TYPE_CHECKING
 from typing import cast
 
@@ -49,7 +49,7 @@ def get_default_text(spec: SegySpec) -> str:
     text_lines = DEFAULT_TEXT_HEADER_LINES.copy()
 
     # Populate write time
-    now = datetime.now(tz=UTC).isoformat(timespec="seconds")
+    now = datetime.now(tz=timezone.utc).isoformat(timespec="seconds")
     text_lines[5] = text_lines[5].format(timestamp=now)
 
     # Populate revision
