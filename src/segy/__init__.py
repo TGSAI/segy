9b1ba8f3e80b874ffd5ef4e06985013b76285261
Amit P <100163178+amitpendharkar@users.noreply.github.com>
2024-07-29 08:31:18 -0500

    Added `__version__` attribute to root package (#155)
    * Fixed segy version issue
    
    * Added a newline
    
    ---------
    
    Co-authored-by: Amit.Pendharkar <Amit.Pendharkar@tgs.com>

--- src/segy/__init__.py
+++ src/segy/__init__.py
@@ -1,6 +1,12 @@
 """'The' SEG-Y implementation."""
+from importlib import metadata
 
 from segy.factory import SegyFactory
 from segy.file import SegyFile
 
 __all__ = ["SegyFile", "SegyFactory"]
+
+try:
+    __version__ = metadata.version("segy")
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
