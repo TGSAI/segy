a9ce4ee89341660b02b5872130456f4e88e39892
Altay Sansal <altay.sansal@tgs.com>
2024-11-20 13:47:06 -0600

    Add logging to SEG-Y standard registry functions

--- src/segy/standards/registry.py
+++ src/segy/standards/registry.py
@@ -2,16 +2,22 @@
 
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from segy.schema import SegySpec
+
+
+logger = logging.getLogger(__name__)
+
 
 segy_standard_registry = {}
 
 
 def register_segy_standard(version_or_name: float | str, spec: SegySpec) -> None:
     """Register a SEG-Y standard with its spec."""
+    logger.info("Registered new SEG-Y spec: %s", version_or_name)
     segy_standard_registry[version_or_name] = spec
 
 
@@ -25,6 +31,7 @@ def get_segy_standard(version_or_name: float | str) -> SegySpec:
             f"would like to use {version_or_name}, please register it with "
             f"the `SegySpecFactory` using its `register_spec` method."
         )
+        logger.error(msg)
         raise NotImplementedError(msg)
 
     return spec.model_copy(deep=True)
